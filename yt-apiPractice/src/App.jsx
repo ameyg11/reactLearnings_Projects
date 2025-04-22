bc57a180f024c9e4751b6211a6cd2ab554c898f9
Amey <ameygawade11@gmail.com>
2025-04-22 20:17:14 +0530

    add to cart mini practice

--- yt-apiPractice/src/App.jsx
+++ yt-apiPractice/src/App.jsx
@@ -1,12 +1,57 @@
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
+const brands = [
+  { id: 1, name: "Nike" },
+  { id: 2, name: "Adidas" },
+  { id: 3, name: "Puma" },
+  { id: 4, name: "Reebok" },
+  { id: 5, name: "Under Armour" },
+  { id: 6, name: "New Balance" },
+  { id: 7, name: "Asics" },
+  { id: 8, name: "Fila" },
+  { id: 9, name: "Converse" },
+  { id: 10, name: "Vans" },
+];
+
+export default function App() {
+  const [selectedItem, setSeletectedItem] = useState([]);
+
+  const onAddToCartClick = (id) => {
+    console.log("Clicked");
+    console.log(id);
+    const selectedBrand = brands.find((brand) => {
+      return brand.id === id;
+    })
+    console.log(selectedBrand.name)
+    setSeletectedItem([...selectedItem, selectedBrand]) 
+  };
+
+  const onRemoveClick = (id) => {
+    const filteredBrand = selectedItem.filter((brand) => brand.id != id)
+    setSeletectedItem(filteredBrand)
+  }
 
   return (
     <>
-      
+      <div>
+        <p>Add to cart</p>
+        {brands.map((brand) => {
+          return (
+            <div key={brand.id}>
+              <span>{brand.name}</span>
+              <button onClick={() => onAddToCartClick(brand.id)}>
+                Add to Cart
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <p>Your Cart</p>
+        {
+          selectedItem.map(brand =><p key={brand.id}>{brand.name} <button onClick={() => onRemoveClick(brand.id)}>Remove</button></p>)
+        }
+      </div>
     </>
-  )
+  );
 }
-
-export default App
